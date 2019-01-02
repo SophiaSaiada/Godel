a32236e7210d9c71e3fad2342737b90e266608d7
YossiSaiada <yossi787@gmail.com>
2019-01-02 10:29:38 +0200

    :heavy_plus_sign: Adding a Kotlin Coroutines :bug: Fixing MainTest

--- src/compiler/src/test/kotlin/MainTest.kt
+++ src/compiler/src/test/kotlin/MainTest.kt
@@ -3,7 +3,6 @@ package com.godel.compiler
 import io.kotlintest.matchers.startWith
 import io.kotlintest.matchers.string.shouldContain
 import io.kotlintest.should
-import io.kotlintest.shouldBe
 import io.kotlintest.specs.StringSpec
 
 class MainTests : StringSpec({
@@ -11,6 +10,5 @@ class MainTests : StringSpec({
         val helloWorld = helloWorld()
         helloWorld should startWith("Hello World!")
         helloWorld shouldContain "Compiler"
-        helloWorld shouldBe "Hello World! (from Compiler)"
     }
 })
