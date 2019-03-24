b2dd541ab6462a24bace804c28fdc73fdcb79713
YossiSaiada <yossi787@gmail.com>
2019-03-24 15:54:18 +0200

    :art: Create a list of concrete ASTNodes.

--- src/compiler/src/main/kotlin/ASTBuilder.kt
+++ src/compiler/src/main/kotlin/ASTBuilder.kt
@@ -1,18 +1,7 @@
 package com.godel.compiler
 
-sealed class ASTNode {
-    abstract val type: NodeType
-}
-
-data class ASTLeaf(val token: Token) : ASTNode() {
-    override val type: TokenType = token.type
-}
-
-data class ASTBranchNode(
-    override val type: InnerNodeType,
-    val children: List<ASTNode>
-) : ASTNode()
-
-
 object ASTBuilder {
+    fun buildAST(rootNode: ParseTreeNode): ASTNode {
+        TODO()
+    }
 }
