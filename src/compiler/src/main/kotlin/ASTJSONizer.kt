67d7be0b7b959192a2d8c3b23ba1e76ea92097e3
YossiSaiada <yossi787@gmail.com>
2019-05-07 00:03:02 +0300

    :bug: Fix Statement.toJson.

--- src/compiler/src/main/kotlin/ASTJSONizer.kt
+++ src/compiler/src/main/kotlin/ASTJSONizer.kt
@@ -5,13 +5,14 @@ object ASTJSONizer {
     private fun Any?.toJSON(): String =
         when (this) {
             is ASTNode.Statement,
-            is ASTNode.Expression,
-            is ASTNode.Statements ->
+            is ASTNode.Expression ->
                 this::class.memberProperties.map {
                     it.name to it.getter.call(this)
                 }.joinToString(", ") { (name, value) ->
                     """"$name": ${value?.toJSON()}"""
                 }.let { "{\"name\": \"${this::class.qualifiedName.orEmpty().removePrefix("com.godel.compiler.ASTNode.")}\", \"props\": {$it}}" }
+            is ASTNode.Statements ->
+                "{\"name\": \"Statements\", \"statements\": ${this.joinToString(", ") { it.toJSON() }.let { "[$it]" }}}"
             is List<Any?> ->
                 this.joinToString(", ") { it.toJSON() }.let { "[$it]" }
             is String -> "\"$this\""
