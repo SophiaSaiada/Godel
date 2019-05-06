dd9b706a576a26016440873cbeb107b7aa07e6ea
YossiSaiada <yossi787@gmail.com>
2019-05-06 23:46:33 +0300

    :sparkles: Add ASTJSONizer to export AST as JSON.

---
+++ src/compiler/src/main/kotlin/ASTJSONizer.kt
@@ -0,0 +1,30 @@
+import com.godel.compiler.ASTNode
+import kotlin.reflect.full.memberProperties
+
+object ASTJSONizer {
+    private fun Any?.toJSON(): String =
+        when (this) {
+            is ASTNode.Statement,
+            is ASTNode.Expression,
+            is ASTNode.Statements ->
+                this::class.memberProperties.map {
+                    it.name to it.getter.call(this)
+                }.joinToString(", ") { (name, value) ->
+                    """"$name": ${value?.toJSON()}"""
+                }.let { "{\"name\": \"${this::class.qualifiedName.orEmpty().removePrefix("com.godel.compiler.ASTNode.")}\", \"props\": {$it}}" }
+            is List<Any?> ->
+                this.joinToString(", ") { it.toJSON() }.let { "[$it]" }
+            is String -> "\"$this\""
+            is Int -> this.toString()
+            is Map<*, *> ->
+                this.map { (key, value) ->
+                    key.toString() to value.toJSON()
+                }.joinToString(", ") { (key, value) ->
+                    """"$key": $value"""
+                }
+            null -> "null"
+            else -> toString()
+        }
+
+    fun toJSON(root: ASTNode.Statements) = root.toJSON()
+}
