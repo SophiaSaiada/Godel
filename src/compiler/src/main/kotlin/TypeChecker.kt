c41512e71caacf8b77d358a1640c517f2a337257
SophiaSaiada <yossi787@gmail.com>
2019-09-11 19:47:50 +0300

    WIP

--- src/compiler/src/main/kotlin/TypeChecker.kt
+++ src/compiler/src/main/kotlin/TypeChecker.kt
@@ -1,2 +1,25 @@
+data class ClassDescription(
+    val name: String,
+    val members: List<Member>
+) {
+    sealed class Member {
+        abstract val name: String
+
+        class Property(
+            override val name: String,
+            val type: ASTNode.Type
+        ) : Member()
+
+        class Method(
+            override val name: String,
+            val parameterTypes: List<ASTNode.Type>,
+            val resultType: ASTNode.Type
+        ) : Member()
+    }
+}
+
 object TypeChecker {
+    fun checkTypes(classRoots: List<ASTNode.ClassDeclaration>): List<ASTNode.ClassDeclaration> {
+
+    }
 }
