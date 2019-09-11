8147164cfd68e32820c79b05ccc4d77ca79f700f
SophiaSaiada <yossi787@gmail.com>
2019-09-11 20:35:37 +0300

    Executor placeholder.

--- src/executor/src/main/kotlin/Executor.kt
+++ src/executor/src/main/kotlin/Executor.kt
@@ -1,4 +1,13 @@
-fun main() {
-    // Just a proof that we cam access classes from :common
-    ASTNode.Identifier("x")
+class Executor(
+    val classes: Map<String, ASTNode.ClassDeclaration>
+) {
+    constructor(classes: Set<ASTNode.ClassDeclaration>) : this(classes.map { it.name to it }.toMap())
+
+    fun run(mainFunction: ASTNode.FunctionDeclaration) {
+        evaluate(mainFunction.body.statements)
+    }
+
+    private fun evaluate(statements: ASTNode.Statements) {
+
+    }
 }
