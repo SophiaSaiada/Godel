d3fe56bbf0e2517b7db427780898235a0ec23b85
KatyaFabrikant <demiluv2008@gmail.com>
2019-09-11 20:56:38 +0300

    main function check!

--- src/compiler/src/main/kotlin/Main.kt
+++ src/compiler/src/main/kotlin/Main.kt
@@ -3,8 +3,20 @@ fun main(args: Array<String>) {
 
 fun compile(sourceCode: Sequence<Char>) {
     val tokenSequence = Lexer.lex(sourceCode)
-    val tokensPerClass = Classifier.classify(tokenSequence)
-    val abstractSyntaxTress =
-        tokensPerClass.map { ASTTransformer.transformAST(Parser.parse(it)) }
-    abstractSyntaxTress.toList()
+    val abstractSyntaxTree =
+        ASTTransformer.transformAST(Parser.parse(tokenSequence))
+    val classDeclarations = mutableListOf<ASTNode.ClassDeclaration>()
+    var mainFunction: ASTNode.FunctionDeclaration? = null
+    for (statement in abstractSyntaxTree) {
+        when (statement) {
+            is ASTNode.ClassDeclaration -> classDeclarations.add(statement)
+            is ASTNode.FunctionDeclaration -> if (statement.name == "main" && mainFunction == null) {
+                mainFunction = statement
+            } else
+                throw ASTError("There needs to be only one function named main!")
+        }
+    }
+    if (mainFunction == null) {
+        throw ASTError("There needs to be at least one function named main!")
+    }
 }
