4b07a1a539df0f0a78347e9d93cbcf8015a6a194
Netanel <netanelbh4321@gmail.com>
2019-09-12 15:58:00 +0300

    TypeChecker finish

--- src/compiler/src/main/kotlin/TypeChecker.kt
+++ src/compiler/src/main/kotlin/TypeChecker.kt
@@ -39,6 +39,63 @@ object TypeChecker {
     }
 
     fun checkTypes(classRoots: List<ASTNode.ClassDeclaration>): List<ASTNode.ClassDeclaration> {
+        val classMemberTypeResolver = object : ClassMemberTypeResolver {
+            override fun resolve(classType: ASTNode.Type, memberName: String, isSafeCall: Boolean): ASTNode.Type {
+                if (classType.nullable && !isSafeCall)
+                    throw ASTError("****")
+                val t = classType as ASTNode.Type.Regular
+                for (c in classRoots)
+                    if (c.name == t.name) {
+                        val classDescription: ClassDescription = getClassDescription(c)
+                        for (member in classDescription.members)
+                            if (member.name == memberName)
+                                return if (member is ClassDescription.Member.Property)
+                                    member.type
+                                else {
+                                    val methodMember: ClassDescription.Member.Method =
+                                        member as ClassDescription.Member.Method
+                                    ASTNode.Type.Functional(
+                                        methodMember.parameterTypes,
+                                        member.resultType,
+                                        member.resultType.nullable
+                                    )
+                                }
+                    }
+                throw ASTError("Class didn't exist")
+            }
 
+            override fun resolve(
+                classType: ASTNode.Type,
+                memberName: String,
+                argumentTypes: List<ASTNode.Type>,
+                isSafeCall: Boolean
+            ): ASTNode.Type.Functional {
+                if (classType.nullable && !isSafeCall)
+                    throw ASTError("****")
+                val t = classType as ASTNode.Type.Regular
+                for (c in classRoots)
+                    if (c.name == t.name) {
+                        val classDescription: ClassDescription = getClassDescription(c)
+                        for (member in classDescription.members)
+                            if (member.name == memberName)
+                                if (member is ClassDescription.Member.Property)
+                                    throw ASTError("the member isn't function")
+                                else {
+                                    val methodMember: ClassDescription.Member.Method =
+                                        member as ClassDescription.Member.Method
+                                    if (argumentTypes == methodMember.parameterTypes)
+                                        return ASTNode.Type.Functional(
+                                            methodMember.parameterTypes,
+                                            member.resultType,
+                                            member.resultType.nullable
+                                        )
+                                    else
+                                        throw ASTError("parameter list doesn't fit")
+                                }
+                    }
+                throw ASTError("Class didn't exist")
+            }
+        }
+        return classRoots.map { it.typed(emptyMap(), classMemberTypeResolver).first }
     }
 }
