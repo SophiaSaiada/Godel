f5e69238334927698b6fd52feb54ba5c35cac436
KatyaFabrikant <demiluv2008@gmail.com>
2019-09-16 09:33:28 +0300

    evalute for statement

--- src/executor/src/main/kotlin/Executor.kt
+++ src/executor/src/main/kotlin/Executor.kt
@@ -8,6 +8,38 @@ class Executor(
     }
 
     private fun evaluate(statements: ASTNode.Statements) {
+        for (statement in statements){
+            evaluate(statement)
+        }
+    }
+
+    private fun evaluate(statement:ASTNode.Statement){
+        when (statement){
+            is ASTNode.Block -> evalute(statement)
+            is ASTNode.ClassDeclaration ->evalute(statement)
+            is ASTNode.Expression ->evalute(statement)
+            is ASTNode.FunctionDeclarationOrValDeclaration->evaluate(statement)
+            is ASTNode.If->evaluate(statement)
+            is ASTNode.Member->evaluate(statement)
+        }
+    }
+
+    private fun evalute(block:ASTNode.Block){
+
+    }
+    private fun evalute(Class:ASTNode.ClassDeclaration){
+
+    }
+    private fun evalute(expression:ASTNode.Expression){
+
+    }
+    private fun evalute(funOrVal:ASTNode.FunctionDeclarationOrValDeclaration){
+
+    }
+    private fun evalute(onlyIf:ASTNode.If){
+
+    }
+    private fun evalute(member:ASTNode.Member){
 
     }
 }
