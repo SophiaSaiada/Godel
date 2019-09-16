e74eff3cea48d6b23d9cb833909238fa4e8e6dff
SophiaSaiada <yossi787@gmail.com>
2019-09-16 09:46:45 +0300

    Rename Main.kt into Compiler.kt and wrap it with Compiler object.

---
+++ src/compiler/src/main/kotlin/Compiler.kt
@@ -0,0 +1,33 @@
+data class CompilationResult(
+    val classDeclarations: List<ASTNode.ClassDeclaration>,
+    val mainFunction: ASTNode.FunctionDeclaration
+)
+
+object Compiler {
+    fun compile(sourceCode: Sequence<Char>): CompilationResult {
+        val tokenSequence = Lexer.lex(sourceCode)
+        val abstractSyntaxTree =
+            ASTTransformer.transformAST(Parser.parse(tokenSequence))
+        val classDeclarations = mutableListOf<ASTNode.ClassDeclaration>()
+        var mainFunction: ASTNode.FunctionDeclaration? = null
+        for (statement in abstractSyntaxTree) {
+            when (statement) {
+                is ASTNode.ClassDeclaration ->
+                    classDeclarations.add(statement)
+                is ASTNode.FunctionDeclaration ->
+                    when {
+                        statement.name != "main" ->
+                            throw CompilationError("Only main function can be a top-level function.")
+                        mainFunction == null ->
+                            mainFunction = statement
+                        else ->
+                            throw CompilationError("There needs to be only one function named main!")
+                    }
+            }
+        }
+        mainFunction?.let {
+            val classDeclarationsWithTypes = TypeChecker.withTypes(classDeclarations)
+            return CompilationResult(classDeclarationsWithTypes, it)
+        } ?: throw CompilationError("There needs to be at least one function named main!")
+    }
+}
